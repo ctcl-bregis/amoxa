cbd0d9a2a062a2933931176866471f01bedffd55
ctcl-bregis <brayden.regis@gmail.com>
2025-03-05 11:49:42 -0500

    20250305-1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# serstrm
+# PyRPG
+An attempt to make some sort of an example of a top-down RPG-like game that makes use of tiles.
---
+++ app.py
@@ -0,0 +1,7 @@
+# pyrpg - CTCL 2025
+# File: app.py
+# Purpose: Import and run game
+# Created: February 22, 2025
+# Modified: February 22, 2025
+
+import src.main
